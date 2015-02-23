c0761eb08848ef90edf52a109e3f4d115cc83502
Nathan Oh <njo@andrew.cmu.edu>
2015-02-22 21:16:31 -0500

    added articles and relationship to category

---
+++ app/models/article2.rb
@@ -0,0 +1,8 @@
+class Article2 < ActiveRecord::Base
+	
+belongs_to :category
+validates_presence_of :title, :content
+scope :alphabetical, -> { order('title') }
+scope :active, -> { where(active: true) }
+
+end
